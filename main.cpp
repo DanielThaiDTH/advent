5b1e5861e2eae36e870baee58a547b7e14b9b382
Daniel Thai <daniel.thai.can@gmail.com>
2021-12-04 01:22:04 -0500

    day 4

--- main.cpp
+++ main.cpp
@@ -7,8 +7,22 @@
 #include <climits>
 #include <cstdio>
 #include <algorithm>
+#include <functional>
+#include <sstream>
 
 static const int BITSIZE = 12;
+
+template<typename T>
+void map(std::vector<T>& vec, std::function<bool(const T&)> f)
+{
+    for (auto it = vec.begin(), it != vec.end();) {
+        if (!f(*it)) {
+            vec.erase();
+        } else {
+            ++it;
+        }
+    }
+}
 
 std::vector<std::string> readFileList(std::string filename)
 {
@@ -31,145 +45,147 @@ std::vector<std::string> readFileList(std::string filename)
 
 enum DIRECTION { FOR, UP, DOWN };
 
-struct Field 
+struct Board
 {
-    bool val[BITSIZE] = {0};
-    void print() 
+    int numbers[5][5] = {};
+    bool matched[5][5] = {};
+    bool isBingo = false;
+    void print()
     {
-        for (bool b : val) {
-            std::cout << b;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                std::cout << numbers[i][j] << " ";
+            }
+            std::cout << std::endl;
         }
+
         std::cout << std::endl;
+    }
+    bool check(int val)
+    {
+        bool rowfound = false;
+        bool colfound = false;
+        
+        if (isBingo)
+            return isBingo;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (!matched[i][j] && val == numbers[i][j]) {
+                    matched[i][j] = true;
+                }
+                colfound = matched[0][j] && matched[1][j] && matched[2][j] && matched[3][j] && matched[4][j];
+                if (colfound)
+                    break;
+            }
+            rowfound = matched[i][0] && matched[i][1] && matched[i][2] && matched[i][3] && matched[i][4]; 
+            if (rowfound)
+                break;
+        }
+
+        isBingo = rowfound || colfound;
+
+        return isBingo;
+    }
+
+    int sum()
+    {
+        int total = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (!matched[i][j]) {
+                    total += numbers[i][j];
+                }
+            }
+        }
+
+        return total;
     }
 };
 
 
-Field parseLine(const char* line)
+std::vector<int> parseCSV(std::string line)
 {
-    Field result;
-
-    for (int i = 0; i < BITSIZE; i++) {
-        result.val[i] = (line[i] == '1');//? 1 : -1;
+    std::istringstream in(line);
+    std::vector<int> nums;
+    int num = 0;
+    char c;
+    while (in >> num) {
+        nums.push_back(num);
+        in >> c;
+        std::cout << num << ", ";
     }
 
-    return result;
+    std::cout << std::endl;
+
+    return nums;
 }
 
+Board* parseBoard(const std::vector<std::string>& lines, int start, int end)
+{
+    Board* b = new Board();
+    for (int i = start + 1; i < end; i++) {
+        std::istringstream row(lines[i]);
+        //std::cout << "Building on line: " << row.str() << std::endl;
+        for (int cnt = 0; cnt < 5; cnt++) {
+            row >> b->numbers[i - 1 - start][cnt];
+        }
+        //std::cout << std::endl;
+    }
+
+    //b->print();
+    return b;
+}
 
 int main()
 {
-    std::vector<std::string> list = readFileList("3/data.txt");
-    std::vector<Field> fields;
-    std::vector<Field> res;
-    std::vector<Field> res2;
-    std::vector<Field> temp;
-    int sumfield = 0;
-    int sumfield2 = 0;
-    int co2 = 0;
-    int o2 = 0;
+    std::ifstream file("4/data.txt");
 
-    for (std::string line : list) {
-        fields.push_back(parseLine(line.c_str()));
-    }
-
-    res = fields;
-    res2 = fields;
-
-    for (int i = 0; i < BITSIZE; i++) {
-        for (const Field& f : res) {
-            sumfield += f.val[i]? 1 : -1; 
-        }
-        for (const Field& f : res2) {
-            sumfield2 += f.val[i]? 1 : -1; 
-        }
-        if (sumfield >= 0) {
-            std::cout << "1 more or equal : o2\n";
-            if (res.size() > 1) {
-                temp = res;
-                res.clear();
-                std::copy_if(temp.begin(), temp.end(), std::back_inserter(res), 
-                                [&](const Field& f) { 
-                                    return f.val[i];
-                                }
-                );
-            }
-        } else {
-            std::cout << "0 more : o2\n";
-            if (res.size() > 1) {
-                temp = res;
-                res.clear();
-                std::copy_if(temp.begin(), temp.end(), std::back_inserter(res), 
-                                [&](const Field& f) { 
-                                    return !f.val[i];
-                                }
-                );
-            }
-        }
-
-        if (sumfield2 >= 0) {
-            std::cout << "0 less or equal : co2\n";
-            if (res2.size() > 1) {
-                temp = res2;
-                res2.clear();
-                std::copy_if(temp.begin(), temp.end(), std::back_inserter(res2), 
-                                [&](const Field& f) { 
-                                    return !f.val[i];
-                                }
-                );
-            }
-        } else {
-            std::cout << "1 less : co2\n";
-            if (res2.size() > 1) {
-                temp = res2;
-                res2.clear();
-                std::copy_if(temp.begin(), temp.end(), std::back_inserter(res2), 
-                                [&](const Field& f) { 
-                                    return f.val[i];
-                                }
-                );
-            }
-        }
-        std::cout << res.size() << " " << res2.size() << std::endl;
-        sumfield = 0;
-        sumfield2 = 0;
-    }
-
-    res[0].print();
-    res2[0].print();
-
-    for (int i = BITSIZE - 1; i >= 0; i--) {
-        if (res[0].val[i]) {
-            o2 += (1 << (BITSIZE - i - 1));
-        }
-        if (res2[0].val[i]) {
-            co2 += (1 << (BITSIZE - i - 1));
-        }
-    }
-
-    std::cout << o2*co2;
-}
-
-// int main()
-// {
-//     std::vector<std::string> list = readFileList("3/data.txt");
-//     Field sum;
-//     int gamma = 0;
-//     int epi = 0;
-
-//     for (std::string line : list) {
-//         Field res = parseLine(line.c_str());
-//         for (int i = 0; i < 12; i++) {
-//             sum.val[i] += res.val[i];
-//         }
-//     }
+    std::string drawnStr;
+    std::string line;
+    std::vector<std::string> lines;
+    std::vector<int> splits;
+    std::vector<int> drawnNums;
+    std::vector<Board*> boards;
+    int count = 0;
     
-//     for (int i = 11; i >= 0; i--) {
-//         if (sum.val[i] >= 0) {
-//             gamma += (1 << (11-i));
-//         } else {
-//             epi += (1 << (11-i));
-//         }
-//     }
+    std::getline(file, drawnStr);
+    drawnNums = parseCSV(drawnStr);
 
-//     std::cout << gamma*epi;
-// }
+    while (std::getline(file, line)) {
+        lines.push_back(line);
+        if (line.size() <= 2) {
+            splits.push_back(count);
+        }
+        count++;
+    }
+
+    std::cout << splits[0] << " "  << splits[1] << " " << splits[2] 
+            << " :: " << splits.size() << std::endl << std::endl;
+
+    for (auto s = 0u; s < splits.size() - 1; s++) {
+        boards.push_back(parseBoard(lines, splits[s], splits[s+1]));
+    }
+    //end board
+    boards.push_back(parseBoard(lines, splits[splits.size() - 1], lines.size()));
+
+    bool bingoCalled = false;
+    for (int num : drawnNums) {
+        for (Board* b : boards) {
+            if (b->isBingo) continue;
+            bool bingo = b->check(num);
+            if (bingo) {
+                std::cout << "Bingo on " << num << ", value: ";
+                std::cout << num*b->sum() << std::endl;
+                bingoCalled = true;
+            }
+        }
+
+        //if (bingoCalled) break;
+    }
+
+    for (Board*& b : boards) {
+        delete b;
+        b = nullptr;
+    }
+}
